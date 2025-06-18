7221d4fb6652add8af521b4cb49e5c887b94ea17
amsharidwan <ridwanamshari0310@gmail.com>
2025-06-18 15:50:46 +0700

    add page router

---
+++ client/src/components/student/TestimonialsSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TestimonialsSection() {
+  return (
+    <div>
+      <h1>Testimonials Section</h1>
+    </div>
+  );
+}
+
+export default TestimonialsSection;
---
+++ client/src/pages/student/MyEnrollments.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MyEnrollments() {
+  return (
+    <div>
+      <h1>My Enrollments Page</h1>
+    </div>
+  );
+}
+
+export default MyEnrollments;
